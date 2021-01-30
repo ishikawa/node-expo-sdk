07ff12f0c8b83b41783babf473d1196ac8f9d410
Takanori Ishikawa <ishikawa_takanori@mac.com>
2021-01-31 07:27:14 +0900

    add jest-setup.js

--- packages/node-expo-random/index.ts
+++ packages/node-expo-random/index.ts
@@ -1,15 +1,10 @@
-// expo-random: nodejs implementations
-jest.mock('expo-random/build/ExpoRandom', () => ({
-  getRandomBytes: jest.fn((byteCount: number) => {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const crypto = require('crypto');
-    const buffer = crypto.randomBytes(byteCount);
-    return new Uint8Array(buffer.buffer);
-  }),
-  getRandomBytesAsync: jest.fn((byteCount: number) => {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const crypto = require('crypto');
-    const buffer = crypto.randomBytes(byteCount);
-    return Promise.resolve(new Uint8Array(buffer.buffer));
-  })
-}));
+import crypto from 'crypto';
+
+export function getRandomBytes(byteCount: number): Uint8Array {
+  const buffer = crypto.randomBytes(byteCount);
+  return new Uint8Array(buffer.buffer);
+}
+
+export function getRandomBytesAsync(byteCount: number): Promise<Uint8Array> {
+  return Promise.resolve(getRandomBytes(byteCount));
+}
